911ced4bb4d083c3ceb1d2e56a35d01e4bafb081
theosykas <theosykas@icloud.com>
2026-03-17 15:39:13 +0100

    output_valid

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from parsing_json import JsonParser
+from parsing_json import JsonParser, JsonWriter, FunctionDefinition
 from init_sdk import init_llm
 import argparse
+import json
 
 
 def main() -> None:
     qwen_model = init_llm(model_name="Qwen/Qwen3-0.6B")
-    print('charged qwen model llm_sdk\n')
-    tokens = qwen_model.encode("hello World\n")
-    print(f'token test {tokens}')
     parse_arg = argparse.ArgumentParser()
     parse_arg.add_argument(
         "--functions_definition",
@@ -22,15 +20,29 @@ def main() -> None:
         default="data/output/function_calls.json"
     )
     args = parse_arg.parse_args()
-    function_json = JsonParser(args.functions_definition)  # default value
-    read_functions = function_json.read_json()
-    reader_json = JsonParser(args.input)
-    read_test = reader_json.read_json()
-    output_create = JsonParser(args.output)
-    output_created = output_create.create_output(read_test)
-
-# writer = JsonParser(args.output)
-# writer.generate_constrain_json(final_output)
+    raw_defs = JsonParser(args.functions_definition).read_json()
+    functions_reader = [FunctionDefinition(**d) for d in raw_defs]
+    catalog = FunctionDefinition.function_catalog(functions_reader)  # tr en catalog pour qwen
+    voc_size = len(qwen_model.get_logits_from_input_ids(qwen_model.encode(" ").tolist()[0]))  # generation all ids token
+    all_token = list(range(voc_size))
+    generate_output = JsonParser(args.input).read_json()
+    with JsonWriter(args.output) as write:
+        for case_input in generate_output:
+            user_prompt = case_input["prompt"]
+            generate_json = FunctionDefinition.generate_constrain_json(
+                llm=qwen_model,
+                user_prompt=user_prompt,
+                catalog=catalog,
+                allowed_token=all_token
+            )
+            try:
+                data_dict = json.loads(generate_json)
+                print('valid Json\n\n')
+                print(generate_json)
+                write.write_json(data_dict)
+            except Exception:
+                print('jsondecode Error\n')
+                print(generate_json)
 
 
 if __name__ == "__main__":
--- parsing_json.py
+++ parsing_json.py
@@ -16,11 +16,14 @@ class FunctionDefinition(BaseModel):
     returns: Dict[str, str] = Field(min_length=1)
 
     @staticmethod
-    def constrain_decoding(llm: Small_LLM_Model, input_ids: List[int],
-                           allowed_token: List[int]) -> List[int]:
-        logits_list = llm.get_logits_from_input_ids(input_ids)  # recuperer tout les logits brut
+    def constrain_decoding(
+        llm: Small_LLM_Model, input_ids: List[int], allowed_token: List[int]
+    ) -> List[int]:
+        logits_list = llm.get_logits_from_input_ids(
+            input_ids
+        )  # recuperer tout les logits brut
         # logits_list = [1, 456, 54]
-        allowed = set(allowed_token)  # opti avant for
+        allowed = set(allowed_token)  # la liste des tokens autorisés à être générés à ce moment précis 1 logits par token
         vocab_size = len(logits_list)
         for mask in range(vocab_size):
             if mask not in allowed:
@@ -29,11 +32,46 @@ class FunctionDefinition(BaseModel):
         next_token_id = logits_list.index(max_logits)
         return next_token_id
 
-    def generate_constrain_json(llm: Small_LLM_Model, user_prompt: str,
-                                allowed_token: List[int]) -> List[str]:
-        current_input = llm.encode(user_prompt).tolist()[0]
+    def format_argument(args: Dict[str, ParamInput]) -> str:
+        return str(list(args.keys()))
+
+    # text_to_prompt = [0] fn.name = fn_add_nb fn.description = fn.decription args = [a, b]
+    def function_catalog(functions: List["FunctionDefinition"]) -> str:
+        catalog_function = ""
+        for i, fn in enumerate(functions):
+            args_list = FunctionDefinition.format_argument(fn.parameters)
+            catalog_function += f"[{i}] {fn.name}: {fn.description}\n"
+            catalog_function += f"Args: {args_list}\n\n"
+        return catalog_function
+
+    def check_brace(count_brace: int, token_str: str) -> int:
+        count_brace += token_str.count("{")
+        count_brace -= token_str.count("}")
+        return count_brace
+
+    # LLM Response Prefixing == systeme_prompt
+    def generate_constrain_json(
+        llm: Small_LLM_Model, user_prompt: str, catalog: str, allowed_token: List[int]
+    ) -> List[str]:
+        formated_ouput = f'{{"prompt": "{user_prompt}", "name": "'
+        # str start: '{' end: '}'
+        systeme_prompt = (
+            "### Role\n"
+            "You are an intelligent API Router. Your goal is to map user requests "
+            "to the exact function and arguments needed.\n\n"
+            "### Available Catalog\n"
+            f"{catalog}"
+            "### Task\n"
+            "Output a single JSON object with the keys 'prompt', 'name', and 'parameters'.\n\n"
+            "### User Request\n"
+            f'"{user_prompt}"\n\n'
+            "### Output\n"
+            f"{formated_ouput}"
+        )
+        current_input = llm.encode(systeme_prompt).tolist()[0]  # [1345, 49, 4907890...]
         generate_ids: List[str] = []
         max_token: int = 128
+        brace_count = 1  # pour la premiere ouverte a 1
         for _ in range(max_token):
             next_id = FunctionDefinition.constrain_decoding(
                 llm, current_input, allowed_token  # who is best -> token
@@ -41,9 +79,14 @@ class FunctionDefinition(BaseModel):
             current_input.append(next_id)
             generate_ids.append(next_id)
             last_token = llm.decode([next_id])
-            if '}' in last_token:
-                break  # soit un , ou } il en faut un pour passer au prochain
-        return llm.decode(generate_ids)  # decode uniquement ce que on a generer
+            brace_count = FunctionDefinition.check_brace(brace_count, last_token)
+            if brace_count == 0:
+                break
+        generated_qwery = llm.decode(
+            generate_ids
+        )  # decode uniquement ce que on a generer
+        final_output = formated_ouput + generated_qwery
+        return final_output
 
 
 class JsonWriter:
@@ -52,21 +95,21 @@ class JsonWriter:
         self.json_file = None
         self.coma = True  # char premier
 
-    def __enter__(self) -> 'JsonWriter':
+    def __enter__(self) -> "JsonWriter":
         self.json_file = open(self.path, "w")
-        self.json_file.write('[\n')
-        return (self)
+        self.json_file.write("[\n")
+        return self
 
-    def __exit__(self, exec_t, exec_v, exec_tb) -> 'JsonWriter':
+    def __exit__(self, exec_t, exec_v, exec_tb) -> "JsonWriter":
         if self.json_file:
-            self.json_file.write('\n]')
+            self.json_file.write("\n]")
             self.json_file.close()
-        return (self)
+        return self
 
-    def write_json(self, data: Dict) -> 'JsonWriter':
-        if self.coma:
-            self.json_file.write(',\n')
-        json_data = json.dumps(data, indent='\t')
+    def write_json(self, data: Dict) -> "JsonWriter":
+        if not self.coma:
+            self.json_file.write(",\n")
+        json_data = json.dumps(data, indent="\t")
         self.coma = False
         self.json_file.write(json_data)
 
@@ -92,10 +135,11 @@ class JsonParser:
         try:
             os.makedirs(os.path.dirname(self.path), exist_ok=True)
             with open(self.path, "w", encoding="UTF-8") as output_file:
-                data_output = json.dump(data_write, output_file, indent='\t')
+                data_output = json.dump(data_write, output_file, indent="\t")
                 return data_output
         except OSError:
             print(f"Directory {self.path} can not be created")
         return None
+
 
 # -inf == (-float('inf')) == 0% float - inf
