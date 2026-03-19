8be038224025dc462d027a03f25d72066c7867d5
theosykas <theosykas@icloud.com>
2026-03-19 19:12:32 +0100

    consrain

--- main.py
+++ main.py
@@ -23,9 +23,12 @@ def main() -> None:
     raw_defs = JsonParser(args.functions_definition).read_json()
     functions_reader = [FunctionDefinition(**d) for d in raw_defs]
     catalog = FunctionDefinition.function_catalog(functions_reader)  # tr en catalog pour qwen
-    voc_size = len(qwen_model.get_logits_from_input_ids(qwen_model.encode(" ").tolist()[0]))  # generation all ids token
-    all_token = list(range(voc_size))
     generate_output = JsonParser(args.input).read_json()
+    voc_size = len(qwen_model.get_logits_from_input_ids(
+        qwen_model.encode(" ").tolist()[0]))  # generation all ids token
+    token_map = {i: qwen_model.decode([i]) for i in range(voc_size)}
+    # valid_choice = [fn.name for fn in functions_reader]
+    JsonParser(args.output).create_ouptut([])
     with JsonWriter(args.output) as write:
         for case_input in generate_output:
             user_prompt = case_input["prompt"]
@@ -33,15 +36,16 @@ def main() -> None:
                 llm=qwen_model,
                 user_prompt=user_prompt,
                 catalog=catalog,
-                allowed_token=all_token
+                functions_reader=functions_reader,
+                token_map=token_map
             )
             try:
                 data_dict = json.loads(generate_json)
                 print('valid Json\n\n')
                 print(generate_json)
                 write.write_json(data_dict)
-            except Exception:
-                print('jsondecode Error\n')
+            except Exception as e:
+                print(f'jsondecode Error {e}\n')
                 print(generate_json)
 
 
--- parsing_json.py
+++ parsing_json.py
@@ -16,17 +16,56 @@ class FunctionDefinition(BaseModel):
     returns: Dict[str, str] = Field(min_length=1)
 
     @staticmethod
+    def get_allowed_token(prefix: str,
+                          valid_token: List[int],
+                          token_map: Dict[int, str],
+                          mode: str = "string") -> List[int]:
+        allowed_token = []
+        for token_id, token_str in token_map.items():  # lit pre calcul dict (map)
+            if mode == "number":
+                numeric_value = set("0123456789.")
+                for c in token_str:
+                    if c not in numeric_value:
+                        break
+                else:
+                    allowed_token.append(token_id)
+            elif mode == "string":
+                allowed_token.append(token_id)
+            else:
+                corresponding = False  # mis a false a chaques iterations
+                combined = prefix + token_str
+                for choice in valid_token:
+                    if choice.startswith(combined):
+                        corresponding = True
+                        break
+                if corresponding:
+                    allowed_token.append(token_id)  # add token id to set allowed token
+                    continue  # -> next_token
+                if token_str == '"':  # end of word
+                    for choice in valid_token:
+                        if prefix == choice:
+                            allowed_token.append(token_id)
+                            break
+        return allowed_token
+
+    @staticmethod
     def constrain_decoding(
-        llm: Small_LLM_Model, input_ids: List[int], allowed_token: List[int]
-    ) -> List[int]:
+        llm: Small_LLM_Model,
+        current_prefix: str,
+        input_ids: List[int],
+        allowed_token: List[str],
+        token_map: Dict[int, str],
+            mode: str = "string") -> List[int]:
         logits_list = llm.get_logits_from_input_ids(
             input_ids
         )  # recuperer tout les logits brut
         # logits_list = [1, 456, 54]
-        allowed = set(allowed_token)  # la liste des tokens autorisés à être générés à ce moment précis 1 logits par token
+        allowed_token = set(allowed_token)
+        token_selection = FunctionDefinition.get_allowed_token(
+            current_prefix, allowed_token, token_map, mode)
         vocab_size = len(logits_list)
         for mask in range(vocab_size):
-            if mask not in allowed:
+            if mask not in token_selection:
                 logits_list[mask] = -float("inf")  # ecrase les scores par -inf
         max_logits = max(logits_list)  # trouve le plus proche du max
         next_token_id = logits_list.index(max_logits)
@@ -47,63 +86,98 @@ class FunctionDefinition(BaseModel):
             catalog_function += f"Args: {args_list}\n"
         return catalog_function
 
-    @staticmethod
-    def check_brace(count_brace: int, token_str: str) -> int:
-        count_brace += token_str.count("{")
-        count_brace -= token_str.count("}")
-        return count_brace
+    # @staticmethod
+    # def check_brace(count_brace: int, token_str: str) -> int:
+    #     count_brace += token_str.count("{")
+    #     count_brace -= token_str.count("}")
+    #     return count_brace
 
-    # LLM Response Prefixing == systeme_prompt
+    @staticmethod
     def generate_constrain_json(
-        llm: Small_LLM_Model, user_prompt: str,
-            catalog: str, allowed_token: List[int]) -> List[str]:
-        user_escape_char = user_prompt.replace('"', '\\"')
-        formated_ouput = f'{{"prompt": "{user_escape_char}", "name": "'
-        # str start: '{' end: '}'
+        llm: Small_LLM_Model,
+        user_prompt: str,
+        catalog: str,
+        functions_reader: List["FunctionDefinition"],
+            token_map: Dict[int, str]) -> str:
+        # user_escape = user_prompt.replace('"', '\\"')
+        formated_ouput = f'{{"prompt": "{user_prompt}", "name": "'
         systeme_prompt = (
             "### Role\n"
-            "You are an intelligent API Router. Your goal is to map user requests "
-            "to the exact function and arguments needed.\n\n"
-            "### Available Catalog\n"
-            f"{catalog}"
-            "### Task\n"
-            "### Formatting Rules\n"
-            "1. Output a single JSON object with the keys 'prompt', 'name', and 'parameters'.\n"
-            "2. TYPE-SPECIFIC RULES:\n"
-            "   - If a parameter is defined as 'number' in the catalog: Use float format (e.g., 2.0, 144.0).\n"
-            "   - If a parameter is defined as 'string' in the catalog: Keep it as a string. NEVER convert text to numbers.\n"
-            "3. Do not add any text or explanation."
+            "You are an intelligent API Router. You map natural language "
+            "requests to the correct Function ID.\n\n"
+            "### Available Tools\n"
+            f"{catalog}\n"
+            "### User Request\n"
             f'"{user_prompt}"\n\n'
+            "### Task\n"
+            "1. Analyze the verbs and nouns in the User Request.\n"
+            "2. Find the 'Action' in the Available Tools "
+            "list that matches the request.\n"
+            "3. Select the corresponding ID number in catalog.\n\n"
             "### Output\n"
             f"{formated_ouput}"
         )
-        current_input = llm.encode(systeme_prompt).tolist()[0]  # [1345, 49, 4907890...]
-        generate_ids: List[str] = []
-        max_token: int = 128
-        brace_count = 1  # pour la premiere ouverte a 1
+        current_input = llm.encode(systeme_prompt + formated_ouput).tolist()[0]
+        generate_ids = []
+        current_prefix = ""
+        fn_names = [fn.name for fn in functions_reader]
+        max_token = 64
         for _ in range(max_token):
             next_id = FunctionDefinition.constrain_decoding(
-                llm, current_input, allowed_token  # who is best -> token
+                llm, current_prefix, current_input, fn_names, token_map, mode="catalog"
             )
             current_input.append(next_id)
             generate_ids.append(next_id)
             last_token = llm.decode([next_id])
-            brace_count = FunctionDefinition.check_brace(
-                brace_count, last_token)
-            if brace_count == 0:
+            if last_token == '"':
                 break
-        generated_qwery = llm.decode(
-            generate_ids
-        )  # decode uniquement ce que on a generer
-        final_output = formated_ouput + generated_qwery
-        return final_output
+            current_prefix += last_token
+        chose_function = next((f for f in functions_reader if f.name == current_prefix), None)
+        if chose_function:
+            param_bridge = ', "parameters": {'
+            bridge_ids = llm.encode(param_bridge).tolist()[0]
+            current_input.extend(bridge_ids)
+            generate_ids.extend(bridge_ids)
+            params = list(chose_function.parameters.items())
+            for i, (p_name, p_info) in enumerate(params):
+                key_str = f' "{p_name}": '
+                if p_info.type == "string":
+                    key_str += '"'
+                key_ids = llm.encode(key_str).tolist()[0]
+                current_input.extend(key_ids)
+                generate_ids.extend(key_ids)
+                current_prefix = ""
+                val_mode = p_info.type
+                # number_max_token = 16
+                for _ in range(max_token):
+                    next_id = FunctionDefinition.constrain_decoding(
+                        llm, current_prefix, current_input, [], token_map, mode=val_mode
+                    )
+                    current_input.append(next_id)
+                    generate_ids.append(next_id)
+                    token_str = llm.decode([next_id])
+                    current_prefix += token_str
+                    if val_mode == "string" and token_str == '"':
+                        break
+                    if val_mode == "number" and "." in current_prefix and current_prefix[-1].isdigit():
+                        break
+                if i < len(params) - 1:
+                    sep = ", "
+                    sep_ids = llm.encode(sep).tolist()[0]
+                    current_input.extend(sep_ids)
+                    generate_ids.extend(sep_ids)
+        closing = "}}"
+        close_ids = llm.encode(closing).tolist()[0]
+        current_input.extend(close_ids)
+        generate_ids.extend(close_ids)
+        return formated_ouput + llm.decode(generate_ids)
 
 
 class JsonWriter:
     def __init__(self, path: str) -> None:
         self.path = path
         self.json_file = None
-        self.coma = True  # char premier
+        self.coma = True
 
     def __enter__(self) -> "JsonWriter":
         self.json_file = open(self.path, "w")
